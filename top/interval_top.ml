7344d00eea38ae37dba51bbd222e3605cab8062e
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2021-01-18 17:13:29 +0100

    Install a small script for the toplevel

---
+++ top/interval_top.ml
@@ -0,0 +1,19 @@
+open Printf
+
+let eval_string
+      ?(print_outcome = false) ?(err_formatter = Format.err_formatter) str =
+  let lexbuf = Lexing.from_string str in
+  let phrase = !Toploop.parse_toplevel_phrase lexbuf in
+  Toploop.execute_phrase print_outcome err_formatter phrase
+
+let load_interval() =
+  let require pkg = eval_string (sprintf "#require %S;;" pkg) in
+  require "interval_base"
+  && require "interval_crlibm"
+  && require "interval_intel"
+  && eval_string "#install_printer Interval.I.pp;;"
+
+
+let () =
+  if not (load_interval()) then
+    Format.eprintf "Problem loading Interval libraries@."
