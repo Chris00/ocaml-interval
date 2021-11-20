8190a489c00b8d6bd4ff657c8adacccf8f838809
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2021-11-20 18:28:31 +0100

    Use different arguments to test the speed of binary operators

--- tests/tests_speed.ml
+++ tests/tests_speed.ml
@@ -2,8 +2,14 @@ open Printf
 open Interval_base
 
 let rnd_values = Array.init 1000 (fun _ -> 1000. -. Random.float 2000.)
+let rnd_values2 = Array.init 1000 (fun _ -> 1000. -. Random.float 2000.)
 
 let rnd_values_I =
+  Array.init 1000 (fun _ ->
+      let x1 = 1000. -. Random.float 2000. and x2 = Random.float 10. in
+      I.v x1 (x1 +. x2))
+
+let rnd_values2_I =
   Array.init 1000 (fun _ ->
       let x1 = 1000. -. Random.float 2000. and x2 = Random.float 10. in
       I.v x1 (x1 +. x2))
@@ -20,10 +26,12 @@ let time_of1 n f =
   done;
   (Unix.times ()).Unix.tms_utime -. top
 
-let time_of2 n f =
+let time_of2 ?(incr=false) n f =
+  let rnd2 = if incr then Array.map (fun x -> x +. 1.) rnd_values
+             else rnd_values2 in
   let top = (Unix.times ()).Unix.tms_utime in
   for _ = 1 to n do
-    Array.iter (fun x -> ignore (f x x)) rnd_values;
+    Array.iter2 (fun x y -> ignore (f x y)) rnd_values rnd2;
   done;
   (Unix.times ()).Unix.tms_utime -. top
 
@@ -35,11 +43,10 @@ let time_of1_I ?(pos=false) n f =
   done;
   (Unix.times ()).Unix.tms_utime -. top
 
-let time_of2_I ?(pos=false) n f =
-  let rnd_I = if pos then rnd_values_pos_I else rnd_values_I in
+let time_of2_I n f =
   let top = (Unix.times ()).Unix.tms_utime in
   for _ = 1 to n do
-    Array.iter (fun x -> ignore (f x x)) rnd_I
+    Array.iter2 (fun x y -> ignore (f x y)) rnd_values_I rnd_values2_I
   done;
   (Unix.times ()).Unix.tms_utime -. top
 
@@ -86,9 +93,11 @@ let () =
        I.(fun x -> x**2), None)
     ];
   let t_create1 = time_of2 n (fun x y -> {low = x;  high = y}) in
-  let t_create2 = time_of2 n I.v in
-  printf "Create: {...}: %f  I.v: %f %.1f×\n\n%!"
-    t_create1 t_create2 (t_create2 /. t_create1);
+  let t_create2 = time_of2 n I.v ~incr:true in
+  let t_create3 = time_of1 n I.singleton in
+  printf "Create: {...}: %f  I.v: %f %.1f×  I.v: %f %.1f×\n\n%!"
+    t_create1 t_create2 (t_create2 /. t_create1)
+    t_create3 (t_create3 /. t_create1);
   printf "       |   Float  |    Fpu        | *_crlibm       |  *_intel\n%!";
   List.iter (speed_cmp1 n) [
       ("sin", sin, Some Fpu.fsin, Cr.sin, Some It.sin);
@@ -103,3 +112,9 @@ let () =
       ("log",   log,   Some Fpu.flog, Cr.log,   Some It.log);
       ("log1p", log1p, None,          Cr.log1p, None);
     ]
+
+
+
+(* Local Variables: *)
+(* compile-command: "dune build tests_speed.exe" *)
+(* End: *)
