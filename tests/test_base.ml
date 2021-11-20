96edb8d4d0a5aa80afdcae5ffb7df28280a9f1bd
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2021-11-20 23:39:12 +0100

    Add tests for sqrt with various rounding modes

--- tests/test_base.ml
+++ tests/test_base.ml
@@ -17,6 +17,28 @@ let () =
   assert(RoundUp.pow_i nan 0 = 1.)
 
 let () =
+  assert(Float.is_nan(RoundUp.sqrt nan));
+  assert(Float.is_nan(sqrt nan));
+  assert(Float.is_nan(RoundDown.sqrt nan));
+  assert(RoundUp.sqrt infinity = infinity);
+  assert(sqrt infinity = infinity);
+  assert(RoundDown.sqrt infinity = infinity);
+  assert(1. /. RoundUp.sqrt 0. = infinity);
+  assert(1. /. sqrt 0. = infinity);
+  assert(1. /. RoundDown.sqrt 0. = infinity);
+  assert(1. /. RoundUp.sqrt (-0.) = neg_infinity);
+  assert(1. /. sqrt (-0.) = neg_infinity);
+  assert(1. /. RoundDown.sqrt (-0.) = neg_infinity);
+  let x = 3.9036066558023176e-154 in
+  assert(RoundUp.sqrt x = 1.9757547053726885e-77);
+  assert(sqrt x = 1.975754705372688e-77); (* round to nearest *)
+  assert(RoundDown.sqrt x = 1.975754705372688e-77);
+  let x = 3.90360665580231694e-154 in
+  assert(RoundUp.sqrt x = 1.975754705372688e-77);
+  assert(sqrt x = 1.975754705372688e-77);
+  assert(RoundDown.sqrt x = 1.97575470537268771e-77)
+
+let () =
   assert(I.(v 2. 2. ** 3 = v 8. 8.));
   assert(I.(v 2. 2. ** (-2) = v 0.25 0.25));
   assert(I.(v 0. 0. ** 0 = one));
